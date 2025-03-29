4a7e42b0876870d6891cce7fdc3678967b963b8b
Victus <dm.77.max@gmail.com>
2025-03-29 16:37:43 +0500

    create filtration admin and user

---
+++ FilaPHP/app/Providers/FilamentServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Providers;
+
+use Filament\Http\Middleware\Authenticate;
+use Illuminate\Support\ServiceProvider;
+
+class FilamentServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
